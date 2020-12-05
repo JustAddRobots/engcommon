3a24db3ac563950d5acc6e64f5c8e74ed1c5e0fc
JustAddRobots <justaddrobots@icloud.com>
2020-12-04 22:53:38 -0800

    ISSUE-019: Added more unit tests

--- tests/unit/test_fileio.py
+++ tests/unit/test_fileio.py
@@ -7,8 +7,9 @@ from engcommon.fileio import write_file
 
 def test_write_file():
     mypid = os.getpid()
+    module_name = inspect.getmodulename(inspect.stack()[1][1])
     func_name = inspect.currentframe().f_code.co_name
-    filename = "/tmp/test/engcommon/{0}-{1}.log".format(func_name, mypid)
+    filename = "/tmp/test/{0}/{1}-{2}.log".format(module_name, func_name, mypid)
     write_file(filename, "test\n", "w")
     with open(filename, "r") as f:
         assert f.read() == "test\n"
--- tests/unit/test_log.py
+++ tests/unit/test_log.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 from engcommon.log import get_formatted_logs
 from engcommon.log import get_std_logger_conf
@@ -15,14 +15,8 @@ def test_get_std_logger_conf():
     assert list(get_std_logger_conf().keys()) == logger_keys
 
 
-def test_get_formatted_logs():
-    var = {
-        "neuromancer": ("The sky above the port was the color of television, "
-                        "tuned to a dead channel."),
-        "exhalation": ("It has long been said that air (which others call argon) "
-                       "is the source of life.")
-    }
-    assert get_formatted_logs(var) == (
+def test_get_formatted_logs(firstlines):
+    assert get_formatted_logs(firstlines) == (
         '### neuromancer ###\n'
         'The sky above the port was the color of television, tuned to a dead channel.\n'
         '### exhalation ###\n'
